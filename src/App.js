497d0eb32df9cf5528208fc98413dc1d64de2dc0
batariloa <batariloa@icloud.com>
2023-02-23 16:33:56 +0100

    GH pages

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useRef, useState } from "react";
 import Chat from "./components/Chat";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://socket-io-chat-live.herokuapp.com");
 
 function App() {
   const [username, setUsername] = useState("");
